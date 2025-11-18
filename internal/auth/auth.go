73629651a23e42e8ca14434631aaef04a47fe605
Henry Moran <hmm@localhost.localdomain>
2025-11-18 10:43:24 -0600

    comment changes

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -2,7 +2,6 @@ package auth
 
 import "net/http"
 
-// Simple API-key auth can be expanded later.
 func RequireKeyMiddleware(apiKey string, next http.HandlerFunc) http.HandlerFunc {
     return func(w http.ResponseWriter, r *http.Request) {
         if apiKey != "" && r.Header.Get("X-API-Key") != apiKey {
--- internal/models/models.go
+++ internal/models/models.go
@@ -1,7 +1,6 @@
 package models
 
 // Placeholder models file
-// (You can add shared structures here if needed later)
 
 type HistoryEntry struct {
     ID       string
